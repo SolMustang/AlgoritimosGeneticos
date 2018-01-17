04b7f510466707164e827437c0d589d559150da9
Daniel Roatt <danielroatt@gmail.com>
2018-01-16 22:12:34 -0200

    Classe Constants criada

---
+++ AlgoritimosGeneticos/Class/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritimosGeneticos.Class
+{
+    public abstract class Constants 
+    {
+        static public int sizeComossomo = 9; 
+        static public int sizePopulacao = 500;
+        static public int functionXSize = (int) Math.Pow(2, sizeComossomo);
+        public static Random random = new Random((int)DateTime.Now.Ticks);
+
+        public static Double Function1(double x) {
+            return (double)(100 + Math.Abs(x * Math.Sin(Math.Sqrt(Math.Abs(x)))));
+        }
+    }
+}
